8a2365d7f9129f09108bda1bebb96ee4ab6bd4da
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-07-26 22:23:10 +0200

    DEV-05: adding styling to the question component

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import './style.css';
 
 const Quesion = ({ question }) => {
+    const publishedAt = moment(question.published_at).format("YYYY-MM-DD HH:mm");
     return (
-        <div className="card">
+        <div className="card question">
             <div className="card-header">
                 Quesion: {question.url.replace("/questions/", "")}
             </div>
             <div className="card-body">
                 <blockquote className="blockquote mb-0">
                     <p>{question.question}</p>
-                    <footer className="blockquote-footer"><cite title="Source Title">Published At: </cite>{question.published_at}</footer>
-                    <Link to={question.url} className="btn btn-primary float-right">View Quesion</Link>
+                    <div>
+                        <footer className="blockquote-footer d-inline"><cite title="Source Title">Published At: </cite>{publishedAt}</footer>
+                        <Link to={question.url} className="btn btn-primary float-right d-inline">View Quesion</Link>
+                    </div>
                 </blockquote>
             </div>
         </div>
